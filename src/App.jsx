2f4cb222f887076876600025024a1231dd0ca391
Rafael Ramires <moreira.ramires@gmail.com>
2021-03-12 13:59:58 -0300

    Git explorer finished

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { RepostirotyList } from "./components/RepositoryList"
+import "./styles/global.scss"
+import {RepositoryList} from "./components/RepositoryList"
+
+
+export function App(){
+ 
+  return (
+  <>
+    <RepositoryList/>
+  </>
+  )
+}
---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,25 @@
+
+interface RepositoryItemProps{
+  
+  repository: {
+    name:string,
+    description:string,
+    html_url:string
+  }
+}
+
+
+export function RepositoryItem(props:RepositoryItemProps){
+ 
+  return(
+   
+    <li>
+      <strong>{props.repository?.name ?? "Default"} </strong>
+      <p>{props.repository?.description ?? "Description , pls"}</p>
+      <a href={props.repository?.html_url ?? ""}>
+        Acessar Repositório
+      </a>
+    </li>
+    
+  )
+}
